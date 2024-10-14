7b4a1a89ce09ed8742669dab44f55b906801c91a
naatii <jcorber855@g.educaand.es>
2024-10-14 18:00:27 +0200

    Agregando tests (No hechos, hay que hacerlos)

--- src/test/java/TestEjercicios_06.java
+++ src/test/java/TestEjercicios_06.java
@@ -1,1 +1,65 @@
+import daw2a.Java_ProgramaBasic_05_0.Ejercicios_05_0;
 import daw2a.Java_ProgramaBasic_06.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EjerciciosAleatoriosBuclesESTest {
+
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio1(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio2(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio3(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio4(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio5(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio6(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio7(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio8(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio9(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio10(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio11(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio12(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio13(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_06.ejercicio14(4, 5);, 0.001);
+    }
+}
