f8672e9bdbd503bf0a98045e29e439f2ea4870e5
naatii <jcorber855@g.educaand.es>
2024-10-14 21:35:02 +0200

    Terminado el ejercicio 1 por Natalia

--- src/main/java/daw2a/Java_ProgramaBasic_05_1/Main_05_1.java
+++ src/main/java/daw2a/Java_ProgramaBasic_05_1/Main_05_1.java
@@ -4,7 +4,8 @@ package daw2a.Java_ProgramaBasic_05_1;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main_05_1 {
     public static void main(String[] args) {
-        Matriz1.ejercicio1();
+
+        Matriz1.imprimirMatriz(Matriz1.inicializarMatriz());
         Matriz2.ejercicio2();
     }
 }
