5406a835ed06a401058201b63f37ec21703a442c
naatii <jcorber855@g.educaand.es>
2024-10-14 18:07:30 +0200

    Agregando tests (No hechos, hay que hacerlos)

--- src/test/java/TestEjercicios_05_0.java
+++ src/test/java/TestEjercicios_05_0.java
@@ -1,1 +1,16 @@
 import daw2a.Java_ProgramaBasic_05_0.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EjerciciosAleatoriosBuclesESTest {
+
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_05_0.ejercicio15(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_05_0.ejercicio16(4, 5);, 0.001);
+    }
+}
--- src/test/java/TestEjercicios_05_1.java
+++ src/test/java/TestEjercicios_05_1.java
@@ -1,2 +1,18 @@
 
+import daw2a.Java_ProgramaBasic_05_0.Ejercicios_05_0;
 import daw2a.Java_ProgramaBasic_05_1.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EjerciciosAleatoriosBuclesESTest {
+
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_05_1.ejercicio1(4, 5);, 0.001);
+    }
+    @Test
+    public void testMultiplicar() {
+        assertEquals(20, Ejercicios_05_1.ejercicio2(4, 5);, 0.001);
+    }
+}
